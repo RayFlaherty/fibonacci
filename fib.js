5eb0d5c5b6fc3aa4d081f9c4dbe6d39debcbc842
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-09-12 09:40:44 -0500

    refactoring

--- fib.js
+++ fib.js
@@ -1,14 +1,21 @@
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn");
+const input = document.getElementById("input")
 
 
-btn.onclick = function run (n) {
+function run (n) {
     let arr = [0,1];
     for (let i = 2; i < n + 1; i++) {
         arr.push(arr[i-2] + arr[i-1])
     }
-    //return arr [n]
-    console.log(arr)
+    return arr [n];
+    
+    // console.log(arr)
     }
 
     
-// object.onclick
+btn.onclick = function fib (n) {
+    if (n<2){
+        return n
+    }
+    console.log(fib(n-1) + fib(n-2))
+};
